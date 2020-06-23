d15342c8ac6f4d7f343f1a4a2092885818e6c439
Ayushman Gupta <38486014+ayushman-git@users.noreply.github.com>
2020-06-23 14:32:45 +0530

    Added TopBar, Added increase/decrease font size

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,43 +1,64 @@
 <template>
   <div>
-    <textarea v-model="text" type="text" autofocus />
+    <textarea
+      id="check"
+      v-model="text"
+      v-on:keydown.ctrl.down="decreaseFont"
+      v-on:keydown.ctrl.up="increaseFont"
+      :style="{'font-size': fontSize+'px'}"
+      type="text"
+      autofocus
+    />
+    <TopBar class="topBar" />
     <WordCount class="wordCount" :text="text" />
     <LetterCount class="letterCount" :text="text" />
   </div>
 </template>
 
 <script>
-import WordCount from './WordCount'
-import LetterCount from './LetterCount'
+import WordCount from "./WordCount";
+import LetterCount from "./LetterCount";
+import TopBar from "./TopBar";
 export default {
   name: "Editor",
-  components: { WordCount, LetterCount },
+  components: { WordCount, LetterCount, TopBar },
 
   data() {
     return {
-      text: ""
+      text: "",
+      fontSize: 14
     };
   },
-  methods: {}
+  methods: {
+    decreaseFont() {
+      if (this.fontSize >= 10) this.fontSize -= 2;
+    },
+    increaseFont() {
+      if (this.fontSize <= 26) this.fontSize += 2;
+    }
+  }
 };
 </script>
 
 <style lang="scss" scoped>
+$fontSize: 14px;
+
 div textarea {
   position: relative;
+  font-size: $fontSize;
   color: white;
   height: 100vh;
   width: 100%;
   outline: none;
   border: none;
   margin: 0;
-  padding: 0;
+  padding: 30px 10px 0px 10px;
   resize: none;
   font-family: "Courier New", Courier, monospace;
   background: #0f2027; /* fallback for old browsers */
   background: -webkit-linear-gradient(
     to right,
-    #2c5364,
+    #000000,
     #203a43,
     #0f2027
   ); /* Chrome 10-25, Safari 5.1-6 */
@@ -60,5 +81,13 @@ div textarea {
   position: absolute;
   bottom: 0;
   right: 100px;
+}
+
+.topBar {
+  color: crimson;
+  top: 0;
+  margin: 0;
+  position: absolute;
+  background-color: #0f2027;
 }
 </style>
---
+++ src/components/TopBar.vue
@@ -0,0 +1,41 @@
+<template>
+  <input 
+  type="text" 
+  v-model="headerText" 
+  @blur="onFocusOut"
+  @click="onFocusIn"
+  :style="{'text-align': textAlign}"
+   />
+</template>
+
+<script>
+export default {
+  name: "TopBar",
+  data() {
+    return {
+      headerText: "",
+      textAlign: ""
+    };
+  },
+  methods: {
+        onFocusOut() {
+              this.textAlign = 'center'
+        },
+        onFocusIn() {
+              this.textAlign = 'left'
+        }
+  }
+};
+</script>
+
+<style scoped>
+input {
+  padding: 3px;
+  font-size: 16px;
+  letter-spacing: 2px;
+  text-align: left;
+  border: none;
+  outline: none;
+  width: 100%;
+}
+</style>
